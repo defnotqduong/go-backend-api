c756a4f8777a494edc7feb7fda493ba659e2e619
phanquangduong2002 <phanquangduong2002@gmail.com>
2025-01-02 22:01:20 +0700

    update

--- global/global.go
+++ global/global.go
@@ -3,11 +3,14 @@ package global
 import (
 	"go/go-backend-api/pkg/logger"
 	"go/go-backend-api/pkg/setting"
+
+	"gorm.io/gorm"
 )
 
 var (
 	Config setting.Config
 	Logger *logger.LoggerZap
+	Mdb    *gorm.DB
 )
 
 /*
--- internal/initialize/redis.go
+++ internal/initialize/redis.go
@@ -1,5 +1,26 @@
 package initialize
 
-func InitRedis() {
+import (
+	"context"
+	"go/go-backend-api/global"
 
+	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
+)
+
+var ctx = context.Background()
+
+func InitRedis() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6381",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+		PoolSize: 10,
+	})
+
+	_, err := rdb.Ping(ctx).Result()
+
+	if err != nil {
+		global.Logger.Error("Redis initialization Error: %w", zap.Error(err))
+	}
 }
